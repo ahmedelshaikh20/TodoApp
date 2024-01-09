9f004b03a429b9ed710bf3087d715693948ae3ba
Ahmed Salah <ahmedelshaikh51@gmail.com>
2024-01-09 08:14:10 +0200

    creating splash screen

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,44 +3,64 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.todoapp.ui.screens.SplashScreen
 import com.example.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            TodoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      TodoAppTheme {
+        // A surface container using the 'background' color from the theme
+        Column(
+          modifier = Modifier.fillMaxSize(),
+        ) {
+          Box(modifier = Modifier
+            .fillMaxSize()
+           ) {
+            Image(
+              painter = painterResource(id = R.drawable.shape),
+              contentDescription = "Background Image",
+              Modifier
+                .size(150.dp)
+                .padding(bottom = 50.dp, end = 10.dp)
+                .align(Alignment.TopStart)
+            )
+            SplashScreen()
+
+          }
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    TodoAppTheme {
-        Greeting("Android")
-    }
+  TodoAppTheme {
+    Greeting("Android")
+  }
 }
