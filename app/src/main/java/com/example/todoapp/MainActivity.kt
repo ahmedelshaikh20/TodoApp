b5d3c612b4d96b281bfea0e6276d897529fb3d20
Ahmed Salah <ahmedelshaikh51@gmail.com>
2024-01-11 02:18:10 +0200

    SignUp Screen UI

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.todoapp.ui.screens.SignUpScreen
 import com.example.todoapp.ui.screens.SplashScreen
 import com.example.todoapp.ui.theme.TodoAppTheme
 
@@ -28,10 +37,12 @@ class MainActivity : ComponentActivity() {
         // A surface container using the 'background' color from the theme
         Column(
           modifier = Modifier.fillMaxSize(),
+          verticalArrangement = Arrangement.Center,
+          horizontalAlignment = Alignment.CenterHorizontally
         ) {
-          Box(modifier = Modifier
-            .fillMaxSize()
-           ) {
+          Box(
+            modifier = Modifier.fillMaxWidth()
+          ) {
             Image(
               painter = painterResource(id = R.drawable.shape),
               contentDescription = "Background Image",
@@ -40,9 +51,10 @@ class MainActivity : ComponentActivity() {
                 .padding(bottom = 50.dp, end = 10.dp)
                 .align(Alignment.TopStart)
             )
-            SplashScreen()
 
           }
+          SignUpScreen()
+
         }
       }
     }
