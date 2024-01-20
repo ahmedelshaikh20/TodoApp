b060f4c3c99979b87ff1c107d285ede55a36815b
Ahmed Salah <ahmedelshaikh51@gmail.com>
2024-01-20 22:23:22 +0200

    UserRepository renaming

--- data/src/main/java/com/example/data/repo/UserRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repo/UserRepositoryImpl.kt
@@ -1,19 +1,14 @@
 package com.example.data.repo
 
-import android.annotation.SuppressLint
-import android.util.Log
 import com.example.data.api.user.UserApiClient
+import com.example.data.api.user.toUserInfoModel
 import com.example.domain.models.RegistrationModel
 import com.example.domain.models.SignInModel
 import com.example.domain.models.UserInfoModel
 import com.example.domain.repositories.UserRepo
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.userProfileChangeRequest
 import javax.inject.Inject
-import kotlin.coroutines.suspendCoroutine
 
-class UserRepoImpl @Inject constructor(val userApiClient: UserApiClient) : UserRepo {
+class UserRepositoryImpl @Inject constructor(private val userApiClient: UserApiClient) : UserRepo {
   //Repo Talks to client and then the client make the call
   override suspend fun registerUser(registrationModel: RegistrationModel) {
     userApiClient.registerUser(registrationModel)
@@ -21,9 +16,9 @@ class UserRepoImpl @Inject constructor(val userApiClient: UserApiClient) : UserR
   }
 
 
-  override suspend fun getCurrentUser(): UserInfoModel {
+  override suspend fun getCurrentUser(): UserInfoModel? {
     val firebaseUser = userApiClient.getCurrentUser()
-    return UserInfoModel(firebaseUser?.displayName, firebaseUser?.email)
+    return firebaseUser?.toUserInfoModel()
   }
 
   override suspend fun signInUser(signInModel: SignInModel) {
