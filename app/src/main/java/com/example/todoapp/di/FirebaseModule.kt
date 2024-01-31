267d79ae1d624eb38d9b74ec5d319ba235858401
Ahmed Salah <ahmedelshaikh51@gmail.com>
2024-01-31 16:08:52 +0200

    provide firestore using di

--- app/src/main/java/com/example/todoapp/di/FirebaseModule.kt
+++ app/src/main/java/com/example/todoapp/di/FirebaseModule.kt
@@ -3,6 +3,8 @@ package com.example.todoapp.di
 import com.example.data.api.user.UserApiClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
@@ -16,7 +18,10 @@ object FirebaseModule {
   @Provides
   fun provideFireAuth(): FirebaseAuth {
     return Firebase.auth
-
   }
 
+  @Provides
+  fun provideFireStore(): FirebaseFirestore {
+    return Firebase.firestore
+  }
 }
